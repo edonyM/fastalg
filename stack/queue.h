9aafa2599e381f9d0da5dd64ca96a83898dfce39
edonyM <edonyzpc@gmail.com>
2015-05-19 15:18:37 +0800

    add stack and queue

---
+++ stack/queue.h
@@ -0,0 +1,36 @@
+ /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-05-19 15:00
+ #
+ # Filename: queue.h
+ #
+ # Description: All Rights Are Reserved
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "list.h"
+
+typedef List Queue;
+#define queue_init list_init
+#define queue_destroy list_destroy
+
+int queue_enqueue(Queue *queue, const void **data);  //list_ins_next(queue, queue->tail, data);
+
+int queue_dequeue(Queue *queue, void **data);        //list_rem_next(queue, NULL, data);
+
+#endif
